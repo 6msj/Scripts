54768b44f3bc4ef142a3a095d6817da9ac608e31
James Nguyen <ja.nguyen@gmail.com>
2012-03-25 19:17:11 -0700

    a

--- sshfs_script.py
+++ sshfs_script.py
@@ -3,10 +3,12 @@ import os
 print "Would you like to mount (1) or unmount (2)?"
 choice = int(raw_input("< "))
 if choice == 1:
-    os.system('sshfs james@server:/media/WII/Music/ ~/sshmusic/')
-    os.system('echo "Mount Successful"')
+    return_value = os.system('sshfs james@server:/media/WII/Music/ ~/sshmusic/')
+    if return_value == 0:
+        os.system('echo "Mount Successful"')
 elif choice == 2:
-    os.system('fusermount -u ~/sshmusic/')
-    os.system('echo "Unmount Successful"')
+    return_value = os.system('fusermount -u ~/sshmusic/')
+    if return_value == 0:
+        os.system('echo "Unmount Successful"')
 else:
     print "No valid entry."
